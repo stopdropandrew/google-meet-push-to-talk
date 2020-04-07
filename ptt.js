37602e0c5ca8b29a9b19800b8390fdc43e4d2a91
stopdropandrew <stopdropandrew@gmail.com>
2020-04-07 07:39:10 -0700

    Disable while chat window is open, don't capture all keystrokes

--- ptt.js
+++ ptt.js
@@ -1,6 +1,6 @@
 (() => {
   const toggle = tip => ({ key }) =>
-    key === ' ' && document.querySelectorAll('[data-tooltip]').forEach(el => el.dataset.tooltip.includes(tip) && el.click());
-  document.body.onkeyup = toggle('Turn off microphone');
-  document.body.onkeydown = toggle('Turn on microphone');
+    key === ' ' && !document.querySelector('textarea') && document.querySelectorAll('[data-tooltip]').forEach(el => el.dataset.tooltip.includes(tip) && el.click());
+    document.body.addEventListener('keyup', toggle('Turn off microphone'));
+    document.body.addEventListener('keydown', toggle('Turn on microphone'));
 })();
