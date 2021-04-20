fb4d5efc3d09778e3e3e702a4064e16eefdffd7a
stopdropandrew <stopdropandrew@gmail.com>
2021-04-20 10:54:58 -0700

    Disable 2.0 announcement

--- src/background.js
+++ src/background.js
@@ -58,9 +58,6 @@ const launchRolloutTabIfNeeded = async (reason) => {
     log({ cohort, rolloutValue, launchUrl, launchRolloutCohorts });
     if (cohort >= 0) {
       await setChromeStorage({ hasOpenedRollout: true });
-      chrome.tabs.create({
-        url: `${launchUrl}?ro=${cohort}&v=${__VERSION__}&utm_medium=ext&utm_source=gmptt&utm_campaign=ro${cohort}`,
-      });
       return;
     }
 
